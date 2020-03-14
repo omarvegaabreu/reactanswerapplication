c339b23dd14c0fa8b250bb6318426a72ac4d1d69
omarvegaabreu <omarvega@hotmail.com>
2020-03-14 19:42:50 -0400

    app complete

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,13 +3,16 @@ import Modal from "react-modal";
 
 const OptionModal = props => (
   <Modal
+    ariaHideApp={false} //WORK AROUND FOR NOW *******
     isOpen={!!props.selectedOption}
     contentLabel={"Selected option"}
     onRequestClose={props.handleDeleteModal}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleDeleteModal}>close</button>
+    <h3 className="modal__title" >Selected Option</h3>
+    {props.selectedOption && <p className="modal__body" >{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleDeleteModal}>close</button>
   </Modal>
 );
 
